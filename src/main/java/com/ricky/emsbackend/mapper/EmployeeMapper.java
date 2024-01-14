473be449cc7c9a76503a110ef92b6003907abe1b
Kenna Heng <rickyheng19@gmail.com>
2024-01-13 20:38:56 -0600

    Added employee mapper

---
+++ src/main/java/com/ricky/emsbackend/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.ricky.emsbackend.mapper;
+
+import com.ricky.emsbackend.dto.EmployeeDto;
+import com.ricky.emsbackend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
